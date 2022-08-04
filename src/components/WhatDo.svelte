406f3f8fdb57424bb11c090641de5873f761a7c0
Jeremy Qian <jq357@hotmail.com>
2022-08-03 20:49:22 -0400

    what do

---
+++ src/components/WhatDo.svelte
@@ -0,0 +1,49 @@
+<script>
+</script>
+
+<main>
+    <div class="p-5">
+        <div class="container-fluid">
+            <div class="row featurette">
+                <div class="col-md-7">
+                    <h2 class="featurette-heading fw-normal lh-1">
+                        What Ribbon Does. <span class="text-muted"
+                            >It’s really simple.</span
+                        >
+                    </h2>
+                    <p class="lead">
+                        Ribbon Receipts is a online app that can scan the images
+                        you upload and parse them into readable words
+
+                    </p>
+
+                    <p class="lead">
+                        The stream of words is then read for information that
+                        looks like receipt information and a receipt entry is
+                        created in the database for you to view anytime!
+                    </p>
+                </div>
+                <div class="col-md-5 d-flex justify-content-center">
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="200"
+                    height="200"
+                    fill="currentColor"
+                    class="bi bi-eye"
+                    viewBox="0 0 16 16"
+                >
+                    <path
+                        d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"
+                    />
+                    <path
+                        d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"
+                    />
+                </svg>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<style>
+</style>
--- src/pages/HomePage.svelte
+++ src/pages/HomePage.svelte
@@ -2,11 +2,15 @@
     import Header from "../components/Header.svelte";
     import Logo from "../components/Logo.svelte";
     import Footer from "../components/Footer.svelte";
+    import WhatDo from "../components/WhatDo.svelte";
+
 </script>
 
 <main>
     <Header />
     <Logo />
+    <WhatDo />
+
     <Footer />
 
     <div class="container-fluid" />
