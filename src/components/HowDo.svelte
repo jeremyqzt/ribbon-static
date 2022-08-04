7e63cba9f79301c85a7f7106639b197da0ead14c
Jeremy Qian <jq357@hotmail.com>
2022-08-03 20:56:28 -0400

    how do

---
+++ src/components/HowDo.svelte
@@ -0,0 +1,47 @@
+<script>
+</script>
+
+<main>
+    <div class="p-5">
+        <div class="container-fluid">
+            <div class="row featurette">
+                <div class="col-md-5 d-flex justify-content-center">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="200"
+                        height="200"
+                        fill="currentColor"
+                        class="bi bi-eye"
+                        viewBox="0 0 16 16"
+                    >
+                        <path
+                            d="M12.496 8a4.491 4.491 0 0 1-1.703 3.526L9.497 8.5l2.959-1.11c.027.2.04.403.04.61Z"
+                        />
+                        <path
+                            d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0Zm-1 0a7 7 0 1 0-13.202 3.249l1.988-1.657a4.5 4.5 0 0 1 7.537-4.623L7.497 6.5l1 2.5 1.333 3.11c-.56.251-1.18.39-1.833.39a4.49 4.49 0 0 1-1.592-.29L4.747 14.2A7 7 0 0 0 15 8Zm-8.295.139a.25.25 0 0 0-.288-.376l-1.5.5.159.474.808-.27-.595.894a.25.25 0 0 0 .287.376l.808-.27-.595.894a.25.25 0 0 0 .287.376l1.5-.5-.159-.474-.808.27.596-.894a.25.25 0 0 0-.288-.376l-.808.27.596-.894Z"
+                        />
+                    </svg>
+                </div>
+                <div class="col-md-7">
+                    <h2 class="featurette-heading fw-normal lh-1">
+                        How Ribbon Does It. <span class="text-muted"
+                            >It’s a bit harder now.</span
+                        >
+                    </h2>
+                    <p class="lead">
+                        Ribbon Receipts utilizes Python's Pillow library to convert the receipts into black & white.
+                        It then uses tesseract ocr library to read a image for words. 
+                    </p>
+
+                    <p class="lead">
+                        Once text is extracted, keywords that are common in receipts are picked out as potential canidates for
+                        receipt information and then presented back to you. You can change these fields or leave them as is!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<style>
+</style>
--- src/pages/HomePage.svelte
+++ src/pages/HomePage.svelte
@@ -3,17 +3,17 @@
     import Logo from "../components/Logo.svelte";
     import Footer from "../components/Footer.svelte";
     import WhatDo from "../components/WhatDo.svelte";
-
+    import HowDo from "../components/HowDo.svelte";
 </script>
 
 <main>
     <Header />
     <Logo />
     <WhatDo />
-
+    <hr class="featurette-divider" />
+    <HowDo />
+    <hr class="featurette-divider" />
     <Footer />
-
-    <div class="container-fluid" />
 </main>
 
 <style>
