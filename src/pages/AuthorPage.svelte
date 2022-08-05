fef574965147b13dd034d4cb053e86cdc0c6936a
Jeremy Qian <jq357@hotmail.com>
2022-08-04 20:13:58 -0400

    authors

--- src/pages/AuthorPage.svelte
+++ src/pages/AuthorPage.svelte
@@ -1,11 +1,12 @@
 <script>
     import Header from "../components/Header.svelte";
     import Footer from "../components/Footer.svelte";
+    import Authors from "../components/Authors.svelte"
 </script>
 
 <main>
     <Header />
-
+    <Authors />
     <Footer />
 </main>
 
