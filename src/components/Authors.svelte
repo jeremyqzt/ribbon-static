44057fc4012359863c1fbc283d9bbe0c8c1dfc83
Jeremy Qian <jq357@hotmail.com>
2022-08-06 20:01:10 -0400

    authors

--- src/components/Authors.svelte
+++ src/components/Authors.svelte
@@ -1,82 +1,73 @@
 <div class="container marketing">
-    <!-- Three columns of text below the carousel -->
-    <div class="row">
-        <div class="col-lg-4">
-            <svg
-                class="bd-placeholder-img rounded-circle"
-                width="140"
-                height="140"
-                xmlns="http://www.w3.org/2000/svg"
-                role="img"
-                aria-label="Placeholder: 140x140"
-                preserveAspectRatio="xMidYMid slice"
-                focusable="false"
-                ><title>Placeholder</title><rect
-                    width="100%"
-                    height="100%"
-                    fill="#777"
-                /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text
-                ></svg
-            >
-
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-6">
+            <img
+                src="assets/ben.jpeg"
+                class="rounded-img"
+                alt=""
+                height="150"
+                width="150"
+            />
             <h2 class="fw-normal">Ben Ni</h2>
+            <p>Ben helped with the project.</p>
             <p>
-                Ben is the principle developer for this project.
+                <a
+                    class="btn btn-danger white-text"
+                    href="https://www.linkedin.com/in/ben-ni-98761862/"
+                    ><svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-linkedin"
+                        viewBox="0 0 16 16"
+                    >
+                        <path
+                            d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"
+                        />
+                    </svg> Linkedin</a
+                >
             </p>
-            <p><a class="btn btn-secondary" href="#">View details »</a></p>
         </div>
-        <!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-            <svg
-                class="bd-placeholder-img rounded-circle"
-                width="140"
-                height="140"
-                xmlns="http://www.w3.org/2000/svg"
-                role="img"
-                aria-label="Placeholder: 140x140"
-                preserveAspectRatio="xMidYMid slice"
-                focusable="false"
-                ><title>Placeholder</title><rect
-                    width="100%"
-                    height="100%"
-                    fill="#777"
-                /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text
-                ></svg
-            >
-
-            <h2 class="fw-normal">Heading</h2>
+        <div class="col-md-6" >
+            <img
+                src="assets/jq.jpeg"
+                class="rounded-img"
+                alt=""
+                height="150"
+                width="150"
+            />
+            <h2 class="fw-normal">Jeremy Qian</h2>
+            <p>Jeremy helped with the project.</p>
             <p>
-                Another exciting bit of representative placeholder content. This
-                time, we've moved on to the second column.
+                <a
+                    class="btn btn-danger white-text"
+                    href="https://www.linkedin.com/in/jeremy-qian/"
+                    ><svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-linkedin"
+                        viewBox="0 0 16 16"
+                    >
+                        <path
+                            d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"
+                        />
+                    </svg> Linkedin</a
+                >
             </p>
-            <p><a class="btn btn-secondary" href="#">View details »</a></p>
         </div>
-        <!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-            <svg
-                class="bd-placeholder-img rounded-circle"
-                width="140"
-                height="140"
-                xmlns="http://www.w3.org/2000/svg"
-                role="img"
-                aria-label="Placeholder: 140x140"
-                preserveAspectRatio="xMidYMid slice"
-                focusable="false"
-                ><title>Placeholder</title><rect
-                    width="100%"
-                    height="100%"
-                    fill="#777"
-                /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text
-                ></svg
-            >
-
-            <h2 class="fw-normal">Heading</h2>
-            <p>
-                And lastly this, the third column of representative placeholder
-                content.
-            </p>
-            <p><a class="btn btn-secondary" href="#">View details »</a></p>
-        </div>
-        <!-- /.col-lg-4 -->
     </div>
 </div>
+
+<style>
+    .rounded-img {
+        border-radius: 50%;
+    }
+
+    .white-text {
+        color: white;
+    }
+
+</style>
--- src/pages/AuthorPage.svelte
+++ src/pages/AuthorPage.svelte
@@ -1,12 +1,14 @@
 <script>
     import Header from "../components/Header.svelte";
     import Footer from "../components/Footer.svelte";
-    import Authors from "../components/Authors.svelte"
+    import Authors from "../components/Authors.svelte";
 </script>
 
 <main>
     <Header />
     <Authors />
+    <hr class="featurette-divider" />
+
     <Footer />
 </main>
 
