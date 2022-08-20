884e1e93a5a480738a10560002215c403fdc2cfe
Jeremy Qian <jq357@hotmail.com>
2022-08-20 09:19:59 -0400

    typo

--- src/components/AuthorsLogo.svelte
+++ src/components/AuthorsLogo.svelte
@@ -6,7 +6,7 @@
         <div class="container-fluid py-5">
             <h1 class="display-5 fw-bold">Authors</h1>
             <p class="col-md-8 fs-4">
-                Two People worked on this project - Did it really take two people to build this terrible app? 
+                Two People worked on this project - Did it really take two people to build this terrible web app? 
             </p>
         </div>
     </div>
