1b8533e967678e2f07220c613d3d6531d3772783
Jeremy Qian <jq357@hotmail.com>
2023-03-07 21:59:23 -0500

    add rolling updates

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
-docker buildx build --push --platform=linux/arm64,linux/amd64 -f Docker/static.dockerfile -t jeremyqzt/ribbonstatic .
+VERSION=$(git log -1 --pretty=%h)
 
 # docker build -f Docker/static.dockerfile -t jeremyqzt/ribbonstatic .
+
+eval "docker buildx build --push --platform=linux/arm64,linux/amd64 -f Docker/static.dockerfile -t jeremyqzt/ribbonstatic:$VERSION ."
+
+IMAGE="jeremyqzt/ribbonstatic:$VERSION" envsubst < k8s/ribbonstatic.yaml | kubectl apply -f -
